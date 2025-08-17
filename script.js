42e7b5f7d1544106c93ef26f3a73e11dd30a28e3
Nandu4455 <livio.scherrer1209@gmail.com>
2025-08-17 13:05:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Button Klick
 function begruesse() {
   alert("Yoshi sagt: Hallo! 🐢💚 Willkommen in meiner Welt!");
 }
+
+// Münzen erzeugen
+const coinsContainer = document.querySelector('.coins-container');
+for (let i = 0; i < 10; i++) {
+  const coin = document.createElement('div');
+  coin.classList.add('coin');
+  coin.style.top = `${Math.random() * window.innerHeight}px`;
+  coin.style.left = `${Math.random() * window.innerWidth}px`;
+  coinsContainer.appendChild(coin);
+
+  // Klick auf Münze
+  coin.addEventListener('click', () => {
+    document.getElementById('coinSound').play();
+    coin.style.transform = 'scale(0)';
+    setTimeout(() => coin.remove(), 500);
+  });
+}
+
